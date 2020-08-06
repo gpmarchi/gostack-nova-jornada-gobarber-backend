3ce1cd1e17b3f32a5cfd6534e366b40827274dcc
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-08-06 18:34:47 -0300

    Set the default field value to use uuid generator

--- src/database/migrations/1596730245972-CreateAppointments.ts
+++ src/database/migrations/1596730245972-CreateAppointments.ts
@@ -13,6 +13,7 @@ export default class CreateAppointments1596730245972
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
