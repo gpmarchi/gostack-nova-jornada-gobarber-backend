fe0864ae54c380ab26c177a8fee9617d459a0a42
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-08-09 11:36:33 -0300

    Define application custom error handling class

--- src/services/UpdateUserAvatarService.ts
+++ src/services/UpdateUserAvatarService.ts
@@ -3,6 +3,9 @@ import path from 'path';
 import fs from 'fs';
 
 import uploadConfig from '../config/upload';
+
+import AppError from '../errors/AppError';
+
 import User from '../models/User';
 
 interface Request {
@@ -17,7 +20,10 @@ class UpdateUserAvatarService {
     const user = await usersRepository.findOne(user_id);
 
     if (!user) {
-      throw new Error('Only authenticated users can change their avatar.');
+      throw new AppError(
+        'Only authenticated users can change their avatar.',
+        401,
+      );
     }
 
     if (user.avatar) {
