5f889cd80238bf07adef246566f074c00c0da0d4
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-08-31 15:32:10 -0300

    Configure/use relative imports to base url

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,11 +4,11 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 import routes from './routes';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
 
-import './database';
+import '@shared/infra/typeorm';
 
 const app = express();
 
@@ -34,8 +34,6 @@ app.use(
     });
   },
 );
-
-app.get('/', (request, response) => response.json({ message: 'Hello World' }));
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
