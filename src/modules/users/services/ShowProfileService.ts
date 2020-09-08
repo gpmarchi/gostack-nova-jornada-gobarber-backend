99d8b79c8115a26fd41247a8d329ade962c9c016
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-09-08 18:06:37 -0300

    Add service to show logged in user profile

---
+++ src/modules/users/services/ShowProfileService.ts
@@ -0,0 +1,35 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import IUsersRepository from '../repositories/IUsersRepository';
+
+import User from '../infra/typeorm/entities/User';
+
+interface IRequest {
+  user_id: string;
+}
+
+@injectable()
+class ShowProfileService {
+  private usersRepository: IUsersRepository;
+
+  constructor(
+    @inject('UsersRepository')
+    usersRepository: IUsersRepository,
+  ) {
+    this.usersRepository = usersRepository;
+  }
+
+  public async execute({ user_id }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User not found.');
+    }
+
+    return user;
+  }
+}
+
+export default ShowProfileService;
