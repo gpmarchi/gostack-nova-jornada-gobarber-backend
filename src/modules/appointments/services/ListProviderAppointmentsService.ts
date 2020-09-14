94fa5b0b6a20554c295efd2b3082e13dbd4d5c35
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-09-14 15:35:29 -0300

    List logged provider appoitments in a specific day

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,43 @@
+import { injectable, inject } from 'tsyringe';
+
+import Appointment from '../infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+@injectable()
+class ListProviderAppointmentsService {
+  private appointmentsRepository: IAppointmentsRepository;
+
+  constructor(
+    @inject('AppointmentsRepository')
+    appointmentsRepository: IAppointmentsRepository,
+  ) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public async execute({
+    provider_id,
+    year,
+    month,
+    day,
+  }: IRequest): Promise<Appointment[]> {
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        year,
+        month,
+        day,
+      },
+    );
+
+    return appointments;
+  }
+}
+
+export default ListProviderAppointmentsService;
