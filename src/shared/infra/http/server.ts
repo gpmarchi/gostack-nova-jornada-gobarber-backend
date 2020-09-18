412b84523cc96731f0f16b123e68233ab2724fc4
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-09-18 12:27:09 -0300

    Add helmet and requests rate limiter to express

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,12 +2,14 @@ import 'reflect-metadata';
 import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
+import helmet from 'helmet';
 import cors from 'cors';
 import { errors } from 'celebrate';
 import 'express-async-errors';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middlewares/rateLimiter';
 import routes from './routes';
 
 import '@shared/infra/typeorm';
@@ -15,6 +17,8 @@ import '@shared/container';
 
 const app = express();
 
+app.use(helmet());
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
